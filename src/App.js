6b5e5da975d9dc65d11f8c24176429f8938bc5be
vipinrwt95 <vipinrwt9@gmail.com>
2023-03-31 14:53:40 +0530

    adding features

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 import {useState,useEffect} from 'react'
 import './App.css';
 import Register from './components/Register';
+import Expense from './components/Expense';
+import {Routes,Route} from "react-router-dom"
 
 function App() {
   
   return (
-    <div>
-     <Register />
-    </div>
+   <Routes>
+    
+     <Route path="/" element={<Register/>} />
+     <Route path="/expense" element={<Expense/>} />
+    </Routes>
   );
 }
 
