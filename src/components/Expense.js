48071675bf65129d39d7b9cc3bbf2c86fc6032ad
vipinrwt95 <vipinrwt9@gmail.com>
2023-04-06 21:05:38 +0530

    premium button

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -3,10 +3,12 @@ import axios from "axios";
 import expensepic from "./expense.png"
 import descpic from "./description.png"
 import categorypic from "./category.png"
+import useRazorpay from "react-razorpay";
 
 const baseUrl="http://localhost:3001"
 
 const Expense=()=>{
+   const Razorpay=useRazorpay();
    const expense=useRef();
    const description=useRef();
    const category=useRef(); 
@@ -53,39 +55,63 @@ async function expenseDeleteHandler(id){
        alert(res.data.message);
      
     })
-    await axios.get(`${baseUrl}/Expenses`).then(res=>{
+    await axios.get(`${baseUrl}/Expenses`,{headers:{"Authorization":token}}).then(res=>{
         setallExpenses(res.data)
       })
 }
-return (
-    <div>
-        
+const premiumHandler=async(e)=>{
+   e.preventDefault();
+const token=localStorage.getItem('token');
+const response=await axios.get(`${baseUrl}/purchase/premium`,{headers:{'Authorization':token}})
+var options={
+   "key":response.data.key_id,
+   "order_id":response.data.order.id,
+   "handler":async function(response){
+     
+  await axios.post(`${baseUrl}/purchase/updatetransactions`,{status:true,order_id:options.order_id,payment_id:response.razorpay_payment_id},{headers:{'Authorization':token}})
+         alert('You are a Premium User Now')
+      
+   },
+};
+const rzp1=new Razorpay(options);
+rzp1.on('payment.failed',async function(error){
+   console.log(error)
+   await axios.post(`${baseUrl}/purchase/updatetransactions`,{status:false,order_id:options.order_id},{headers:{'Authorization':token}})
+   alert('Payment failed'); 
   
-         <div className="expenselist"><h1>All Expenses...</h1>
+})
+rzp1.open()
+ 
+
+}
+return (
+    <>
+     <div><button className="prembutton" onClick={premiumHandler} style={{backgroundColor:'yellow',color:'black'}}>Premium</button></div>    
+  <div className="expenselist"><h1>All Expenses...</h1>
             {content}
             </div>
             
         <div>
   <form  onSubmit={ExpenseHandler}> 
   <h1 style={{color:'white',position:'relative' ,left:'80px',top:'2px'}}>New Expense</h1>
-  <div style={{padding:"50px"}}><img src={expensepic} /> <input type="number" className="text" placeholder="Enter Expense.." ref={expense} required/></div>
-  <div style={{padding:"50px"}}><img src={descpic}  /> <input type="text" className="text" placeholder="Description.." ref={description} required/></div>
-  <div style={{padding:"50px"}}><img src={categorypic}  /> <select className="text" ref={category} placeholder="Category" required>
+  <div style={{padding:"20x"}}><img src={expensepic} style={{height:'3rem',width:'3rem'}} /> <input type="number" className="text" placeholder="Enter Expense.." ref={expense} required/></div>
+  <div style={{padding:"20x"}}><img src={descpic}  style={{height:'3rem',width:'3rem'}} /> <input type="text" className="text" placeholder="Description.." ref={description} required/></div>
+  <div style={{padding:"20px"}}><img src={categorypic}  style={{height:'3rem',width:'3rem'}} /> <select className="text" ref={category} placeholder="Category" required>
  <option value="RENT">RENT</option>  
  <option value="FUEL">FUEL</option> 
  <option value="SHOP">SHOPPING</option> 
  <option value="ENTERTAINMENT">ENTERTAINMENT</option>
 </select></div>
-<button className="btn" style={{position:'relative' ,left:'150px'}} type="submit">ADD EXPENSE</button>
+<button className="btn" style={{position:'relative' ,top:'40px',left:'150px' ,width:'125px'}} type="submit">ADD EXPENSE</button>
    </form> 
-   <div style={{position:'absolute',top:'0',right:'0'}}>
-            <div className="list" id="list"><h1>Current Expense Added</h1>
+   <div className='currentexpense'>
+            <div><h1>Current Expense Added</h1>
             {currentExpense && <li>{currentExpense.expense} paid for {currentExpense.description} - {currentExpense.category}</li>}
             </div>
         </div>
    </div>
    
-   </div>
+   </>
 )
 
 
